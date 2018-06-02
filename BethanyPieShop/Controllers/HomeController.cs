e2d4b707e65063042df40293ae4864e3c9b39c77
Jay1305 <nowhere>
2018-06-01 20:59:46 -0700

    Code with MVC

---
+++ BethanyPieShop/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanyPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            var pies = _pieRepository.GetAllPies().OrderBy(d => d.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Bethany's Pie shop",
+                Pies = pies.ToList()
+            };
+            return View(model: homeViewModel);
+        }
+    }
+}
